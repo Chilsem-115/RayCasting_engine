0eb6bf0fa6864214904aa286b36abccb9a25fc29
shadowman <tamsamani.ibrahim115@gmail.com>
2025-09-19 14:50:59 +0100

    welp

--- engine/core/actions.h
+++ engine/core/actions.h
@@ -30,5 +30,24 @@ typedef struct s_actions
 	t_action_state	state[ACTION_MAX];
 }	t_actions;
 
+/* lifecycle */
+void	action_init();
+void	action_clear();
+
+/* action registration*/
+t_action_id	actions_register(t_actions *A, const char *name);
+
+/* binding helpers */
+void	actions_bind_key(t_actions *A, t_action_id id, int key, t_action_trigger trig, int mods);
+void	actions_bind_axis_keys(t_actions *A, t_action_id id, int key_pos, int key_neg, int mods);
+
+/* per-frame handling */
+void	actions_begin_frame(t_actions *A);
+void	actions_update_from_input(t_actions *A, const t_input *I);
+
+/* queries */
+float	action_value(const t_actions *A, t_actions_id id);
+int		action_pressed(const t_actions *A, t_actions_id id);
+int		action_released(const t_actions *A, t_actions_id id);
 
 #endif
